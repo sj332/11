90036e800fb375e7e157b72ebc9fd79b86d6711b
sj332 <lsj02@sookmyung.ac.kr>
2022-11-17 16:29:43 +0900

    01

---
+++ main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	srand((unsigned)time(NULL));	
+	
+	printf("==============================\n");
+	printf("******************************\n");
+	printf("          BINGO GAME          \n");
+	printf("******************************\n");
+	printf("==============================\n\n\n");
+	
+	
+	//generate numbers
+	//initialize bingo boards
+	//while (game is not end)
+	/*
+	{
+		//bingo board printing
+		//print no. of completed lines
+		//sele
+		}
+	*/
+	
+	
+	printf("\n\n\n\n\n\n\n\n\n");
+	printf("==============================\n");
+	printf("******************************\n");
+	printf("       CONGRATULATION!!!      \n");
+	printf("      (*   BINGO!!!!  #)      \n");
+	printf("          YOU WIN!!!!         \n");
+	printf("******************************\n");
+	printf("==============================\n\n");
+
+	
+	
+	
+	
+	return 0;
+}
